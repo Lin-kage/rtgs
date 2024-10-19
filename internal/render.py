5eaa83412e705487fc17c9f543bcdd8e49957fce
Lin-kage <3497976821@qq.com>
2024-10-19 14:55:21 +0800

    generalize ray_trace function

--- internal/render.py
+++ internal/render.py
@@ -2,22 +2,28 @@ import torch
 from .utils import get_eta_autograd, get_eta_manual
 
 
-def ray_trace(gaussians, rays_o, rays_dir, d_s, N_samples, auto_grad=False):
-    
-    d_s = torch.tensor(d_s).to("cuda")
+def ray_trace(rays_o : torch.Tensor, rays_dir : torch.Tensor, d_s, d_steps, lum_field_fn, eta_field_fn, auto_grad=False):
     
     rays_points = []
     rays_dir = rays_dir / rays_dir.norm(dim=-1, keepdim=True)
-    for _ in range(N_samples):
+    for _ in range(d_steps):
         rays_points.append(rays_o)
         
         if auto_grad:
-            etas, d_etas = get_eta_autograd(gaussians, rays_o)
+            with torch.autograd():
+                rays_o_grad = rays_o.clone().requires_grad_(True)
+                etas = eta_field_fn(rays_o_grad)
+                d_etas = torch.autograd.grad(etas.sum(), rays_o_grad)
         else:
-            etas, d_etas = get_eta_manual(gaussians, rays_o)
+            etas, d_etas = eta_field_fn(rays_o)
         
         rays_o = rays_o + rays_dir / etas[:, None] * d_s
         rays_dir = rays_dir + d_etas * d_s
         rays_dir = rays_dir / rays_dir.norm(dim=-1, keepdim=True)
-
-    return torch.cat(rays_points, dim=0)
+        
+    rays_points = torch.cat(rays_points, dim=0)
+    rays_lum = lum_field_fn(rays_points).reshape(rays_o.shape[0], -1).sum(dim=-1)
+    
+    return rays_lum
+        
+    
